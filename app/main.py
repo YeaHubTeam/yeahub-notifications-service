6d1896db94f5c90bdf40e87d69cbee5d553fc70b
YeaHubTeam <ruslan.kuyanets@gmail.com>
2026-01-17 00:55:18 +0300

    delete unused code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
-from app.api.routes.api import router as api_router
-from app.core.config import API_PREFIX, DEBUG, MEMOIZATION_FLAG, PROJECT_NAME, VERSION
-from app.core.events import create_start_app_handler
 from fastapi import FastAPI
+from app.api.router import router as api_router
 
+app = FastAPI()
 
-def get_application() -> FastAPI:
-    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
-    application.include_router(api_router, prefix=API_PREFIX)
-    application.add_event_handler("startup", create_start_app_handler(application))
-    return application
-
-
-app = get_application()
+# Весь API под /v1
+app.include_router(api_router, prefix="/v1")
